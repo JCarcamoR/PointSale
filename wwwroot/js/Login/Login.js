7b3eb55db0554cd85d35df6f1260ce673467580a
JCARCAMO_TEL <carlos.carcamor@outlook.com>
2024-06-11 00:51:04 -0600

    Se agrega funcionalidad de JS a formulario de Login

---
+++ wwwroot/js/Login/Login.js
@@ -0,0 +1,39 @@
+﻿
+var btnRetLogin = document.getElementById("btnReturLog");
+btnRetLogin.addEventListener("click", function () {
+    CleanForms();
+    let SecLog = document.getElementById("SecLogin");
+    SecLog.style.display = "block";
+    let SecCto = document.getElementById("SecContacto");
+    SecCto.style.display = "none";
+});
+
+var linkContacto = document.getElementById("lknCto");
+linkContacto.addEventListener("click", function () {
+    CleanForms();
+    let SecLog = document.getElementById("SecLogin");
+    SecLog.style.display = "none";
+    let SecCto = document.getElementById("SecContacto");
+    SecCto.style.display = "block";
+});
+
+function CleanForms() {
+    $('#UserLogin').val('');
+    $('#password').val('');
+    $('#CteNombre').val('');
+    $('#CteApe').val('');
+    $('#CteEmail').val('');
+    $('#CteTel').val('');
+    $('#CteOpOtro').val('');
+    $('#CteDesc').val('');
+    $('#CteAs').val('1');
+}
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
