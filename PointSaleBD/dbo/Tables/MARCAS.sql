d0dd9d62944368783f63d44f0583efd56e371b90
JCARCAMO_TEL <carlos.carcamor@outlook.com>
2024-06-12 16:12:00 -0600

    Cambios iniciales para el desarrollo de la logica del sistema

--- PointSaleBD/dbo/Tables/MARCAS.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[MARCAS] (
-    [IdMarca]    INT           NULL,
-    [DesMarca]   VARCHAR (100) NULL,
-    [IdEstado]   INT           NULL,
-    [Modificado] DATETIME      NULL,
-    [Creado]     DATETIME      NULL
-);
-
