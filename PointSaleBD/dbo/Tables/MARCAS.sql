ff3c7c4c7b812611fc7a2eb4099d51b910ba594b
JCARCAMO_TEL <carlos.carcamor@outlook.com>
2024-06-12 12:04:34 -0600

    Se agrega proyecto par alojar los objetos de la BD y archivo de definicion de tablas.

---
+++ PointSaleBD/dbo/Tables/MARCAS.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MARCAS] (
+    [IdMarca]    INT           NULL,
+    [DesMarca]   VARCHAR (100) NULL,
+    [IdEstado]   INT           NULL,
+    [Modificado] DATETIME      NULL,
+    [Creado]     DATETIME      NULL
+);
+
