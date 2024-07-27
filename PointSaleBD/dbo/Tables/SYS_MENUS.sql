0975a3f43d235b8f7432849894fcf5be0fe6ee56
JCARCAMO_TEL <carlos.carcamor@outlook.com>
2024-07-26 21:37:57 -0600

    Se actualiza modelo de base de datos

---
+++ PointSaleBD/dbo/Tables/SYS_MENUS.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[SYS_MENUS] (
+    [MEN_PK_CVE]        INT           IDENTITY (1, 1) NOT NULL,
+    [MEN_DS_TITULO]     VARCHAR (200) NULL,
+    [MEN_DS_ICONO]      VARCHAR (500) NULL,
+    [MEN_DS_RUTA]       VARCHAR (MAX) NULL,
+    [MEN_NUM_NIVEL]     INT           NULL,
+    [MEN_CVE_PADRE]     INT           NULL,
+    [MEN_NUM_ORDEN]     INT           NULL,
+    [EST_PK_CVE]        INT           NULL,
+    [MEN_US_MODIFICADO] VARCHAR (20)  NULL,
+    [MEN_FE_ULT_MOD]    DATETIME      NULL,
+    [MEN_FE_CREADO]     DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([MEN_PK_CVE] ASC)
+);
+
+
+
---
+++ PointSaleBD/dbo/Tables/SYS_OPERACIONES.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[SYS_OPERACIONES] (
+    [OPE_PK_CVE]        INT          IDENTITY (1, 1) NOT NULL,
+    [PRM_PK_CVE]        INT          NULL,
+    [ACC_PK_CVE]        INT          NULL,
+    [EST_PK_CVE]        INT          NULL,
+    [OPE_US_MODIFICADO] VARCHAR (20) NULL,
+    [OPE_FE_ULT_MOD]    DATETIME     NULL,
+    [OPE_FE_CREADO]     DATETIME     NULL,
+    PRIMARY KEY CLUSTERED ([OPE_PK_CVE] ASC)
+);
+
+
+
