a5ab7a9674a3c731c651fbc936b2949607c17bde
JCARCAMO_TEL <carlos.carcamor@outlook.com>
2024-07-26 22:33:02 -0600

    SE CAMBIA ESTRUCTURA DE BASE DE DATOS

--- PointSaleBD/dbo/Tables/TIPO_MOVIMIENTOS.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[TIPO_MOVIMIENTOS] (
-    [IdTipoMov]  INT IDENTITY(1,1)  NOT NULL,
-    [DescMov]    VARCHAR (100)      NULL,
-    [IdEstado]   INT                NULL,
-    [Modificado] DATETIME           NULL,
-    [Creado]     DATETIME           NULL,
-    PRIMARY KEY CLUSTERED ([IdTipoMov] ASC)
-);
-
